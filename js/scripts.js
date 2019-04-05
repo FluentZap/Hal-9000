f517eff04d7f0965c66510f6fe5fa14edfc518f8
Nathan <FluentZap@Gmail.com>
2019-04-05 09:04:07 -0700

    add responce box

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
+var newResponceCountTo = 0;
+var newResponceCounter = 0;
+
+
 $(document).ready(function () {
+  $("#halResponse").text("I am putting myself to the fullest possible use, which is all I think that any conscious entity can ever hope to do.")
+  $("#halResponse").fadeIn(1000);
+  //setTimeout(newResponce, 1000);
 
-
-  
 });
+
+
+
+function newResponce() {
+  if (newResponceCounter >= newResponceCountTo) {
+
+    newResponceCountTo = 5 + Math.round(Math.random() * 8)
+  } else newResponceCounter++
+}
